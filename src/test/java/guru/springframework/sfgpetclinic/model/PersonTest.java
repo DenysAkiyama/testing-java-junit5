cc39961eebe606711dab1441cffcda45b076d3fe
denys <denys.akiyama@gmail.com>
2020-05-21 20:58:32 -0300

    Cap48: JUnit Asserções Agrupadas

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+	@Test
+	void groupedAssertions() {
+		//given
+		Person person = new Person(1L, "João", "Batista");
+		
+		//then
+		assertAll("Teste de set de properties(props)",
+				() -> assertEquals(person.getFirstName(), "João"),
+				() -> assertEquals(person.getLastName(), "Batista"));
+	}
+
+	@Test
+	void groupedAssertionsMsgs() {
+		//given
+		Person person = new Person(1L, "João", "Batista");
+		
+		//then
+		assertAll("Teste de set de properties(props)",
+				() -> assertEquals(person.getFirstName(), "João", "### Nome com falha!!! ###"),
+				() -> assertEquals(person.getLastName(), "Batista", "### Sobrenome com falha!!! ###"));
+	}
+}
