90eb47ffa3a2b013d561dcb2fbfa283e91d0eed6
denys <denys.akiyama@gmail.com>
2020-05-27 15:20:26 -0300

    Cap74: JUnit Testes parametrizados - CSV File Source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -80,4 +81,10 @@ class OwnerTest implements ModelTests{
 		System.out.println(stateName + " = " + val1 + " : " + val2);
 	}
 
+	@DisplayName("CSV Test From File")
+	@ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+	@CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+	void csvFromFileTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = " + val1 + " : " + val2);
+	}
 }
