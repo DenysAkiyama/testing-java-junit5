9c99125f38aebb86ce9488c2d321bb6a8ff8c0d1
denys <denys.akiyama@gmail.com>
2020-05-27 14:57:53 -0300

    Cap72: JUnit Testes Parametrizados - ENUM Source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import guru.springframework.sfgpetclinic.ModelTests;
@@ -58,6 +59,13 @@ class OwnerTest implements ModelTests{
 	@ValueSource(strings = {"Spring", "Framework", "Guru"})
 	void testValueSource(String val) {
 		System.out.println(val);
+	}
+	
+	@DisplayName("Enum Source Test ")
+	@ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+	@EnumSource(OwnerType.class)
+	void enumTest(OwnerType ownerType) {
+		System.out.println(ownerType);
 	}
 
 }
