22b998f93dc1fe0e22800610e4896dc7daf7c6eb
denys <denys.akiyama@gmail.com>
2020-05-27 17:16:47 -0300

    Cap77: IT Suffix for Integration Tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,44 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PetTypeSDJpaServiceIT {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testPetTypeSDJpaService() {
+
+	}
+
+	@Test
+	void testFindAll() {
+
+	}
+
+	@Test
+	void testFindById() {
+
+	}
+
+	@Test
+	void testSave() {
+
+	}
+
+	@Test
+	void testDelete() {
+
+	}
+
+	@Test
+	void testDeleteById() {
+
+	}
+
+}
